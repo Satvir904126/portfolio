5949524976ffd49ec099524c1070906600e9199a
Satvir Singh Satvir Singh <ssatvirs@isimtl.local>
2020-07-06 23:45:46 -0400

    Updates

---
+++ precache-manifest.e54f3ae1c3bd80ed0a770c53debd6867.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2624ba2a4016321c7fda361107df712",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "5ab92d98e97be12bcb24",
+    "url": "/portfolio/static/css/main.c1de13fd.chunk.css"
+  },
+  {
+    "revision": "e86eeb02cee2ea046299",
+    "url": "/portfolio/static/js/2.30580de4.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/portfolio/static/js/2.30580de4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5ab92d98e97be12bcb24",
+    "url": "/portfolio/static/js/main.c8b7488e.chunk.js"
+  },
+  {
+    "revision": "e1738373808577e178c6",
+    "url": "/portfolio/static/js/runtime-main.5c010819.js"
+  },
+  {
+    "revision": "45bfd6094cfba525e186f1ffe2f251b3",
+    "url": "/portfolio/static/media/coading1.45bfd609.jpg"
+  },
+  {
+    "revision": "1834a5b6c791ba2fa2feaf204b282257",
+    "url": "/portfolio/static/media/coading2.1834a5b6.jpg"
+  },
+  {
+    "revision": "550f7fa07b79e8f0d49fcefafca5a1a6",
+    "url": "/portfolio/static/media/coading3.550f7fa0.jpg"
+  },
+  {
+    "revision": "b7331e4a0acbc53a4d7430dfde2928fe",
+    "url": "/portfolio/static/media/navlogo.b7331e4a.png"
+  },
+  {
+    "revision": "90967d0dae3a6783e219800d33045fe4",
+    "url": "/portfolio/static/media/profilePic.90967d0d.JPG"
+  }
+]);
